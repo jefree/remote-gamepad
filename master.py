a8fd4d82caef8db83c618e8ad9a7d46a30ff1ea0
jefree <jgarzon920429@gmail.com>
2016-04-17 17:27:29 -0500

    add master auto discover slave

--- master.py
+++ master.py
@@ -1,23 +1,57 @@
 import socket
 import json
+import threading
+import time
+import datetime
 
-if starting:
-  SLAVE_IP = "192.168.0.10"
-  SLAVE_PORT = 6005
+CONNECTION_REQUEST = "REMOTEPADCONNECTIONREQUEST"
+CONNECTION_ACCEPTED = "REMOTEPADCONNECTIONACCEPTED"
+MASTER_IP = socket.gethostbyname(socket.gethostname())
+SLAVE_IP = ""
+PORT = 6009
+
+slave_founded = False
+
+xbox1 = xbox360[0]
+xbox2 = xbox360[1]
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind((MASTER_IP, PORT))
   
-  xbox1 = xbox360[0]
-  xbox2 = xbox360[1]
-  udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def look_for_slave():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('', 0))
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+	while not slave_founded:
+		diagnostics.debug("trying find slave on %s" % PORT)
+		s.sendto(CONNECTION_REQUEST, ('<broadcast>', PORT))
+		time.sleep(5)
+
+def wait_for_slave():
+	global slave_founded, SLAVE_IP
+	
+	while True:
+		data, addr = udp.recvfrom(1024)
+		
+		if (data == CONNECTION_ACCEPTED):
+			diagnostics.debug("slave founded")
+			slave_founded = True
+			SLAVE_IP = addr[0]
+			break
+	
+	run()
+
+def run():
+ 	while True:
+		update()
+		time.sleep(0.015)
   
 def update():
-  global udp
-  global SLAVE_IP
-  global SLAVE_PORT
-  
   statuses = getStatusForXboxControls([xbox1, xbox2])
   data = json.dumps(statuses)
   
-  udp.sendto(data, (SLAVE_IP, SLAVE_PORT))
+  udp.sendto(data, (SLAVE_IP, PORT))
 
 def getStatusForXboxControls(controls):
   return [getXboxStatus(control) for control in controls]
@@ -29,6 +63,8 @@ def getXboxStatus(xbox):
   status["b"] = xbox.b
   status["x"] = xbox.x  
   status["y"] = xbox.y
+  
+  diagnostics.debug(status["a"])
   
   status["rb"] = xbox.rightShoulder 
   status["rt"] = xbox.rightTrigger
@@ -51,6 +87,22 @@ def getXboxStatus(xbox):
   status["back"] = xbox.back
   status["start"] = xbox.start
   
-  return status
+  diagnostics.debug(datetime.datetime.now())
   
-update()
+  return status
+
+if starting:
+
+  t1 = threading.Thread(target=look_for_slave)
+  t2 = threading.Thread(target=wait_for_slave)
+	
+  t1.setDaemon(True)
+  t2.setDaemon(True)
+	
+  t1.start()
+  t2.start()
+
+def pront():
+	diagnostics.debug("hola")
+
+pront()
